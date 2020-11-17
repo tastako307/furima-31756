7fe9c8a1e9b4fa5b5b6494e62d1b417decb1bfc4
tastako307 <tastako307@gmail.com>
2020-11-17 11:50:33 +0900

    user_validates ver1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :encrypted_password, format: { with: /\A([a-z]{1, 127})([\d]{1, 127})\z/, message: "アルファベット・数字を最低でも１文字以上入れてください"}
   validates :nickname,           presence: true
-  validates :first_name,         presence: true
-  validates :last_name,          presence: true
-  validates :first_name_kana,    presence: true
-  validates :last_name_kana,     presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A(ァ-ンー)+\z/, message: '全角カタカナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  
   validates :birthday,           presence: true
 
 end
