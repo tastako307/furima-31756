3d4e9c2f8912c6c9a7f367b22b0e45957bcbced9
tastako307 <tastako307@gmail.com>
2020-11-17 16:31:03 +0900

    user_validates ver1.3
    カタカナのバリデーションを訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email,              uniqueness: {message: "指定したメールアドレスは既に登録されています"}           
-  validates :encrypted_password, format: { with: /\A([a-z]{1, 127})([\d]{1, 127})\z/, message: "アルファベット・数字を最低でも１文字以上入れてください"}
+  validates :encrypted_password, format: { with: /\A([a-z]{1, 127})([\d]{1, 127})\z/, message: "パスワードにはアルファベット・数字を最低でも１文字以上入れてください"}
   validates :nickname,           presence: {message: "ニックネームを入力してください"}
   validates :birthday,           presence: {message: "誕生日を入力してください"}
 
@@ -11,7 +11,7 @@ class User < ApplicationRecord
     validates :last_name
   end
 
-  with_options presence: true,   format: { with: /\A(ァ-ンー)+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true,   format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
