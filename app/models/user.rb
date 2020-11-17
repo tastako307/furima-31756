9e1bb002e8e435a294c58d081d1a3cfae465efce
tastako307 <tastako307@gmail.com>
2020-11-17 11:08:44 +0900

    user_validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :encrypted_password, format: { with: /\A([a-z]{1, 127})([\d]{1, 127})\z/, message: "アルファベット・数字を最低でも１文字以上入れてください"}
+  validates :nickname,           presence: true
+  validates :first_name,         presence: true
+  validates :last_name,          presence: true
+  validates :first_name_kana,    presence: true
+  validates :last_name_kana,     presence: true
+  validates :birthday,           presence: true
+
 end
