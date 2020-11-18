bd5bbe443e3c9e46801d4d170af2affc74ff77ab
tastako307 <tastako307@gmail.com>
2020-11-18 14:45:43 +0900

    正常系、異常系（Email〜Passwordまで）

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
     validates :last_name
   end
 
-  with_options presence: true,   format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true,   format: { with: /\A[ァ-ンー]+\z/, message: '全角カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  after do
+    if @user.errors.any?
+      puts @user.errors.full_messages
+      puts @user.password
+    end
+  end
+  describe 'ユーザー新規登録' do
+    it '@userが正常に登録できる' do
+      expect(@user).to be_valid
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'emailが重複すると登録できない' do
+      user = FactoryBot.build(:user)
+      user.email = @user.email
+      @user.save
+      user.valid?
+      expect(user.errors.full_messages).to include("Email 指定したメールアドレスは既に登録されています")
+    end
+    it 'passwordが空では登録できない' do
+      @user.password = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it 'passwordが数字だけでは登録できない' do
+      @user.password = "12345678"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password パスワードには最低でもアルファベットと数字を一文字づつ含めてください")
+    end
+    it 'passwordがアルファベットだけでは登録できない' do
+      @user.password = "asdfghjk"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password パスワードには最低でもアルファベットと数字を一文字づつ含めてください")
+    end
+    it 'passwordが一致していないと登録できない' do
+      @user.password = "aaaaa"
+      @user.valid?
+      binding.pry
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    it '' do
+    end
+  end
 end
