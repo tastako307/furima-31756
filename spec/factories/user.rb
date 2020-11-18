f255eea5abd7170a7b0051443e230c9efad23710
tastako307 <tastako307@gmail.com>
2020-11-18 14:44:08 +0900

    テストコード準備

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,14 @@
-Factorybot.define do
+FactoryBot.define do
+Faker::Config.locale = 'ja'
   factory :user do
     nickname                {Faker::Internet.username(specifier: 5..8)}
     email                   {Faker::Internet.free_email}
     password                {Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation   {password}
-    last_name               {Faker_ja::Name.last_name}
-    first_name              {Faker_ja::Name.last_name}
-    last_name_kana          {Faker_ja::Games::Pokemon.name}
-    first_name_kana         {Faker_ja::Games::Pokemon.name}
-    birthday                {Faker::Date.between_exept(from: '1945-01-01', to: '2005-12-31')}
+    last_name               {Faker::Name.last_name}
+    first_name              {Faker::Name.first_name}
+    last_name_kana          {Faker::Games::Pokemon.name}
+    first_name_kana         {Faker::Games::Pokemon.name}
+    birthday                {Faker::Date.between_except(from: '1945-01-01', to: '2005-12-31', excepted: '1990-01-24')}
   end
 end
