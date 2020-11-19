1364d9b866ab7c89be8b4aa1ebb73c80ce804375
tastako307 <tastako307@gmail.com>
2020-11-19 14:20:03 +0900

    rollback&migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,18 @@
 ActiveRecord::Schema.define(version: 2020_11_12_121753) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "category_id", null: false
+    t.integer "send_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "fee_id", null: false
+    t.integer "price", null: false
+    t.string "name", null: false
+    t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
