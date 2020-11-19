7b33c0293fc91626438477018e816414329b04cf
tastako307 <tastako307@gmail.com>
2020-11-19 12:14:53 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,18 @@
 class ItemsController < ApplicationController
   def index
-    
   end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 end
