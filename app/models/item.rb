94e9c071575041d0c6ed0f1171584a59cfdad451
tastako307 <tastako307@gmail.com>
2020-11-20 17:55:03 +0900

    バリデーションの訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,10 +16,11 @@ class Item < ApplicationRecord
     greater_than_or_equal_to: 300,\
     less_than_or_equal_to: 9999999,\
     message: '価格の入力は、300〜9999999の範囲内の整数かつ、半角英数字で行う必要があります' }
-  with_options presence: { message: '入力は必須です' } do
-    validates :name
-    validates :text
-  end
+
+  validates :name,  presence: { message: '商品名の入力は必須です' } ,\
+                    length: { maximum: 40, message: '商品名に入力できるのは40文字までです'}
+  validates :text,  presence: { message: '説明文の入力は必須です' } ,\
+                    length: { maximum: 1000, message: '説明文に入力できるのは1000文字までです' }
 
   with_options presence: { message: 'プルダウンから選択して値を入力してください' } do
     validates :deadline_id
