4589e32a76ffe416a7f730907f46254eaf44feb2
tastako307 <tastako307@gmail.com>
2020-11-20 18:14:36 +0900

    テストコード・ファクトリーボットのの記述

---
+++ spec/factories/items.rb
@@ -0,0 +1,20 @@
+FactoryBot.define do
+  Faker::Config.locale = 'ja'
+  factory :item do
+    
+    name            {Faker::Device.manufacturer}
+    text            {Faker::Ancient.hero}
+    price           {Faker::Number.within( range: 300..9999999 )}
+    category_id     {Faker::Number.within( range: 1..10 )}
+    condition_id    {Faker::Number.within( range: 1..6 )}
+    deadline_id     {Faker::Number.within( range: 1..3 )}
+    fee_id          {Faker::Number.within( range: 1..2 )}
+    prefecture_id   {Faker::Number.within( range: 1..47 )}
+    association  :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+  end
+end
