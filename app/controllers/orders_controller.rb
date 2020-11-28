80cafff6c0098bd96a4373e173c0ea320ff63931
tastako307 <tastako307@gmail.com>
2020-11-28 20:33:55 +0900

    決済機能サーバーサイド実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order = OrderSeparate.new(order_params)
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else
@@ -22,7 +23,18 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_separate).permit(:prefecture_id, :post_number, :address_1, :address_2,\
-      :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+      :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+    
   end
 
   def check_order
--- app/models/order_separate.rb
+++ app/models/order_separate.rb
@@ -1,13 +1,14 @@
 class OrderSeparate
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :prefecture_id, :post_number, :address_1, :address_2,\
-               :building, :phone_number
+               :building, :phone_number, :token
 
-  validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/, message: '例：123-3456 郵便番号は左のように入力してください。なおこの項目の入力は全て半角で入力を行います'}
+  validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/, message: '例：123-3456 郵便番号は左のように入力してください。なお、この項目の入力は全て半角で入力を行います'}
   validates :prefecture_id, presence: { message: "都道府県の入力は必須です" }
   validates :address_1, presence: { message:  "市区町村の入力は必須です"}
   validates :address_2, presence: { message: "番地の入力は必須です" }
-  validates :phone_number, format: { with: /\A0[0-9]{9,10}\z/, message: '電話番号は、0から始まる10桁、または11桁の半角数字で入力してください'}
+  validates :phone_number, format: { with: /\A0[0-9]{9,10}\z/, message: '電話番号は、0から始まる10桁、または11桁の半角数字だけで入力してください'}
+  validates :token, presence: { message: 'クレジットカードカードの情報は半角数字で入力する必要があります'}
 
   def save
     Order.create( user_id: user_id, item_id: item_id )
