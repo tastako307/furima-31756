a7cf4eff32175dd261df7fc6a5bad984e48e77df
tastako307 <tastako307@gmail.com>
2020-11-29 11:40:05 +0900

    @item定義の一括化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :item_find, only: [:index, :create, :pay_item, :check_order]
   before_action :authenticate_user!, only: [:index, :create]
   before_action :check_order, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order = OrderSeparate.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order = OrderSeparate.new(order_params)
     if @order.valid?
       pay_item
@@ -26,8 +25,11 @@ class OrdersController < ApplicationController
                                            :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
-  def pay_item
+  def item_find
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
@@ -37,7 +39,6 @@ class OrdersController < ApplicationController
   end
 
   def check_order
-    @item = Item.find(params[:item_id])
     redirect_to root_path if @item.order.presence || current_user.id == @item.user.id
   end
 end
